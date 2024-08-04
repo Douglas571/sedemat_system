181592b1370c5fef481ac09f97296fb14a660a6c
Douglas Socorro <juancarlossocorro571@gmail.com>
2024-08-04 19:57:17 -0400

    Added: Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,30 +8,42 @@ import BusinessViewDetails from 'pages/BusinessViewDetails'
 
 import BranchOfficeLicenseNew from 'pages/BranchOfficeLicenseNew'
 
-import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Outlet, Link, BrowserRouter } from 'react-router-dom'
 
+import MainLayout from 'layout/main'
 
 function App(): JSX.Element {
 	return (
 		<div>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<Home />} />
-						<Route path="payments" element={<ViewPayments />} />
-						<Route path="payments/new" element={<NewPaymentForm />} />					
+					<Route path='/' element={<MainLayout />}>
+						{/* <Route index element={<Home />} /> */}
 
-						<Route path='business' element={<BusinessView/>}/>
-						<Route path='business/new' element={<BusinessNew/>}/>
-						<Route path='business/edit/:businessId' element={<BusinessEdit/>}/>
-						<Route path='business/:businessId' element={<BusinessViewDetails/>}/>
-						<Route path='business/:businessId/branch-office/:branchOfficeId/license/new' element={<BranchOfficeLicenseNew/>}/>
-						
+						<Route index element={<Home />} />
+
+						<Route path='payments' element={<ViewPayments />} />
+						<Route path='payments/new' element={<NewPaymentForm />} />
+
+						<Route path='business' element={<BusinessView />} />
+						<Route path='business/new' element={<BusinessNew />} />
+						<Route
+							path='business/edit/:businessId'
+							element={<BusinessEdit />}
+						/>
+						<Route
+							path='business/:businessId'
+							element={<BusinessViewDetails />}
+						/>
+						<Route
+							path='business/:businessId/branch-office/:branchOfficeId/license/new'
+							element={<BranchOfficeLicenseNew />}
+						/>
 
 						{/* Using path="*"" means "match anything", so this route
 						acts like a catch-all for URLs that we don't have explicit
-						routes for. */}	
-						<Route path="*" element={<NoMatch />} />
+						routes for. */}
+						<Route path='*' element={<NoMatch />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
@@ -41,56 +53,56 @@ function App(): JSX.Element {
 
 function Layout() {
 	return (
-	  <div>
-		{/* A "layout route" is a good place to put markup you want to
+		<div>
+			{/* A "layout route" is a good place to put markup you want to
 			share across all the pages on your site, like navigation. */}
-		<nav>
-		  <ul>
-			<li>
-			  <Link to="/">Inicio</Link>
-			</li>
-			<li>
-			  <Link to="/payments">Pagos</Link>
-			</li>
-			<li>
-			  <Link to="/payments/new">Nuevo Pago</Link>
-			</li>
-			<li>
-				<Link to="/business/">Contribuyentes</Link>
-			</li>
-			<li>
-				<Link to="/business/new">Nuevo Contribuyente</Link>
-			</li>
-		  </ul>
-		</nav>
-  
-		<hr />
-  
-		{/* An <Outlet> renders whatever child route is currently active,
+			<nav>
+				<ul>
+					<li>
+						<Link to='/'>Inicio</Link>
+					</li>
+					<li>
+						<Link to='/payments'>Pagos</Link>
+					</li>
+					<li>
+						<Link to='/payments/new'>Nuevo Pago</Link>
+					</li>
+					<li>
+						<Link to='/business/'>Contribuyentes</Link>
+					</li>
+					<li>
+						<Link to='/business/new'>Nuevo Contribuyente</Link>
+					</li>
+				</ul>
+			</nav>
+
+			<hr />
+
+			{/* An <Outlet> renders whatever child route is currently active,
 			so you can think about this <Outlet> as a placeholder for
 			the child routes we defined above. */}
-		<Outlet />
-	  </div>
-	);
-  }
+			<Outlet />
+		</div>
+	)
+}
 
-  function Home() {
+function Home() {
 	return (
-	  <div>
-		<h2>Inicio</h2>
-	  </div>
-	);
-  }
-  
-  function NoMatch() {
+		<div>
+			<h2>Inicio</h2>
+		</div>
+	)
+}
+
+function NoMatch() {
 	return (
-	  <div>
-		<h2>Nothing to see here!</h2>
-		<p>
-		  <Link to="/">Go to the home page</Link>
-		</p>
-	  </div>
-	);
-  }
+		<div>
+			<h2>Nada que ver aquí!</h2>
+			<p>
+				<Link to='/'>Volver al inicio</Link>
+			</p>
+		</div>
+	)
+}
 
 export default App
---
+++ frontend/src/layout/main.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import {
+	AppstoreOutlined,
+	BarChartOutlined,
+	CloudOutlined,
+	ShopOutlined,
+	TeamOutlined,
+	UploadOutlined,
+	UserOutlined,
+	VideoCameraOutlined
+} from '@ant-design/icons'
+import type { MenuProps } from 'antd'
+import { Layout, Menu, theme } from 'antd'
+
+import {
+	Routes,
+	Route,
+	Outlet,
+	Link,
+	BrowserRouter,
+	useNavigate
+} from 'react-router-dom'
+
+const { Header, Content, Footer, Sider } = Layout
+
+const siderStyle: React.CSSProperties = {
+	overflow: 'auto',
+	height: '100vh',
+	position: 'fixed',
+	insetInlineStart: 0,
+	top: 0,
+	bottom: 0,
+	scrollbarWidth: 'thin',
+	scrollbarColor: 'unset'
+}
+
+// const items: MenuProps['items'] = [
+//   UserOutlined,
+//   VideoCameraOutlined,
+//   UploadOutlined,
+//   BarChartOutlined,
+//   CloudOutlined,
+//   AppstoreOutlined,
+//   TeamOutlined,
+//   ShopOutlined,
+// ].map((icon, index) => ({
+//   key: String(index + 1),
+//   icon: React.createElement(icon),
+//   label: `nav ${index + 1}`,
+// }));
+
+/**
+
+index
+register
+tax_collection
+
+
+ */
+
+const items: MenuProp['items'] = [
+	{
+		key: '/',
+		icon: '',
+		label: 'Inicio',
+		route: 'home'
+	},
+	{
+		key: '/business',
+		icon: '',
+		label: 'Registro'
+	},
+	{
+		key: '/collection',
+		icon: '',
+		label: 'Recaudación'
+	}
+]
+
+const App: React.FC = () => {
+	const navigate = useNavigate()
+	const {
+		token: { colorBgContainer, borderRadiusLG }
+	} = theme.useToken()
+
+	const onClick: MenuProps['onClick'] = data => {
+		//     console.log({data})
+		navigate(data.key)
+	}
+
+	return (
+		<Layout hasSider>
+			<Sider style={siderStyle}>
+				<div className='demo-logo-vertical' />
+				<Menu
+					theme='dark'
+					mode='inline'
+					defaultSelectedKeys={['4']}
+					items={items}
+					onClick={onClick}
+				/>
+			</Sider>
+			<Layout style={{ marginInlineStart: 200 }}>
+				{/* <Header style={{ padding: 0, background: colorBgContainer }} /> */}
+
+
+				<Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+					<Outlet />
+				</Content>
+
+
+
+				<Footer style={{ textAlign: 'center' }}>
+					SEDEMAT Zamora ©{new Date().getFullYear()} Creado por Douglas Socorro
+				</Footer>
+			</Layout>
+		</Layout>
+	)
+}
+
+export default App
