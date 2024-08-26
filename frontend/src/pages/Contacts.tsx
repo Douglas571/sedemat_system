1f372fe63f4c8a89b4abeb3330fd6f0bc7a38b8f
Douglas Socorro <juancarlossocorro571@gmail.com>
2024-08-25 22:20:12 -0400

    Added: Search to contacts table

--- frontend/src/pages/Contacts.tsx
+++ frontend/src/pages/Contacts.tsx
@@ -1,9 +1,13 @@
-import {useState, useEffect} from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-import { Form, Input, Button, message, Typography, Select, Flex, Image, Table} from 'antd'
+import { Form, Input, Button, message, Typography, Select, Flex, Image, Table, Space} from 'antd'
 import { Link, useNavigate } from 'react-router-dom'
 import { Person } from 'util/api'
 import * as api from 'util/api'
+
+import { SearchOutlined } from '@ant-design/icons';
+
+import Highlighter from 'react-highlight-words';
 
 export default function Contacts(): JSX.Element {
 
@@ -25,6 +29,77 @@ export default function Contacts(): JSX.Element {
         }
     }
 
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef<InputRef>(null);
+
+
+    const handleSearch = (
+        selectedKeys: string[],
+        confirm: FilterDropdownProps['confirm'],
+        dataIndex: DataIndex,
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters: () => void) => {
+        clearFilters();
+        setSearchText('');
+    };
+    const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<DataType> => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Buscar
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            clearFilters && handleReset(clearFilters)
+                            confirm({ closeDropdown: false });
+                            close()
+                        }}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Limpiar
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+            <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                .toString()
+                .toLowerCase()
+                .includes((value as string).toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text, record) =>
+            (<Link to={`/contacts/${record.id}`}>{record.firstName + " " + record.lastName}</Link>)
+    });
+
 
     const columns = [
         {
@@ -33,8 +108,8 @@ export default function Contacts(): JSX.Element {
             key: 'fullName',
             showSorterTooltip: false,
             sortDirections: ['ascend', 'descend', 'ascend'],
-            render: (text: string, record: Person) => <Link to={`/contacts/${record.id}`}>{record.firstName + " " + record.lastName}</Link>,
             sorter: (a: Person, b: Person) => a.firstName?.localeCompare(b.firstName),
+            ...getColumnSearchProps('fullName'),
         },
         {
             title: 'Cédula',
