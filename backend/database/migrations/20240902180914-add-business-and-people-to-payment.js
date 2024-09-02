4c87331d674300bd78344fbb7fa74aae7d79af4d
douglas571 <douglassocorro.work@gmail.com>
2024-09-02 14:47:29 -0400

    Added: Migration for new business and person columns

---
+++ backend/database/migrations/20240902180914-add-business-and-people-to-payment.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+
+    await queryInterface.addColumn('Payments', 'businessId', {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'Businesses', // Name of the related table
+          key: 'id'
+      },
+      // onDelete: 'CASCADE',
+      // onUpdate: 'CASCADE',
+    },
+    {
+      name: 'paymentBusinessIdFK'
+    })
+
+    await queryInterface.addColumn('Payments', 'personId', {
+      type: Sequelize.INTEGER,
+      //allowNull: false, // just for now...
+      references: {
+        model: 'People',
+        key: 'id'
+      }
+    },
+    {
+      name: 'paymentPersonIdFK'
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeColumn('Payments', 'businessId', { name: 'paymentBusinessIdFK' })
+    await queryInterface.removeColumn('Payments', 'personId', { name: 'paymentPersonIdFK' })
+    
+  }
+};
