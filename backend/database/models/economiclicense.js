2c4571ab6d100ffee936c83cbd69c99c96057069
douglas571 <douglassocorro.work@gmail.com>
2024-09-06 16:02:04 -0400

    Fixed: Typo

--- backend/database/models/economiclicense.js
+++ backend/database/models/economiclicense.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      const {Invoice, User} = models
+      const {Invoice, User, Business} = models
 
 
       // economic license belongs to one invoice
