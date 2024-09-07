2d1b39476ee15e5b900a382d8d2086ecc9c6f67e
douglas571 <douglassocorro.work@gmail.com>
2024-09-07 16:15:20 -0400

    Feature: Added controls to register a new gross income tax

--- frontend/src/pages/TaxCollectionBusinessGrossIncomes.tsx
+++ frontend/src/pages/TaxCollectionBusinessGrossIncomes.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import { Form, Input, DatePicker, InputNumber, Select, Button, Typography, Card } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, DatePicker, InputNumber, Select, Button, Typography, Card, Flex, Checkbox, Upload } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
 const { Title } = Typography;
 const { Option } = Select;
+
+import dayjs from 'dayjs';
 
 const TaxCollectionBusinessGrossIncomesEdit: React.FC = () => {
     const [form] = Form.useForm();
@@ -31,56 +33,126 @@ const TaxCollectionBusinessGrossIncomesEdit: React.FC = () => {
         navigate(-1);
     };
 
+    // i should get the list of branch offices for a business 
+    // with that list, i will generate a list of options for the branch office select
+
+    const [branchOffices, setBranchOffices] = useState<BranchOffice[]>([]);
+
+    useEffect(() => {
+        loadBranchOffices();
+    }, []);
+
+    async function loadBranchOffices() {
+        // Dummy data for branch offices
+        const dummyBranchOffices: BranchOffice[] = [
+            { id: 1, nickname: "Sucursal Principal", address: "Av. Libertador 123", shouldChargeWasteCollection: true },
+            { id: 2, nickname: "Sucursal Centro", address: "Calle Bolívar 456", shouldChargeWasteCollection: false },
+            { id: 3, nickname: "Sucursal Norte", address: "Av. Miranda 789", shouldChargeWasteCollection: true },
+            { id: 4, nickname: "Sucursal Sur", address: "Calle Sucre 101", shouldChargeWasteCollection: false },
+            { id: 5, nickname: "Sucursal Este", address: "Av. Francisco de Miranda 202", shouldChargeWasteCollection: true },
+        ];
+
+        // Set the branch offices state with dummy data
+        setBranchOffices(dummyBranchOffices);
+    }
+
+    useEffect(() => {
+        // update default values for branch office select 
+        const firstOffice = branchOffices[0];
+
+        if (firstOffice) {
+            form.setFieldsValue({
+                branchOffice: firstOffice.nickname + ' - ' + firstOffice.address,
+                chargeWasteCollection: firstOffice.shouldChargeWasteCollection
+            });
+        }
+    }, [branchOffices]);
+
+    // Update the Select component to use the branchOffices state
+    const branchOfficeOptions = branchOffices.map(office => ({
+        key: office.id,
+        value: `${office.nickname} - ${office.address}`,
+        label: `${office.nickname} - ${office.address}`
+    }));
+
     return (
-        <Card title={<Title level={2}>Agregar Ingreso Bruto</Title>}>
+        <Card title={<Title level={2}>Registrando Ingresos Brutos</Title>}>
             <Form
                 form={form}
                 layout="vertical"
                 onFinish={onFinish}
+                initialValues={{
+                    period: dayjs().subtract(1, 'month'),
+                    amount: 0
+                }}
             >
                 <Form.Item
-                    name="year"
-                    label="Año"
-                    rules={[{ required: true, message: 'Por favor ingrese el año' }]}
-                >
-                    <DatePicker picker="year" />
-                </Form.Item>
-
-                <Form.Item
-                    name="month"
-                    label="Mes"
-                    rules={[{ required: true, message: 'Por favor seleccione el mes' }]}
-                >
-                    <Select>
-                        {Object.entries(monthMapper).map(([key, value]) => (
-                            <Option key={key} value={Number(key)}>{value}</Option>
-                        ))}
-                    </Select>
-                </Form.Item>
-
-                <Form.Item
-                    name="grossIncome"
-                    label="Ingreso Bruto"
-                    rules={[{ required: true, message: 'Por favor ingrese el ingreso bruto' }]}
-                >
-                    <InputNumber
-                        style={{ width: '100%' }}
-                        formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
-                        parser={(value) => value!.replace(/\$\s?|(,*)/g, '')}
-                        prefix="Bs."
-                    />
-                </Form.Item>
-
-                <Form.Item
+                    layout='horizontal'
                     name="branchOffice"
                     label="Sucursal"
                     rules={[{ required: true, message: 'Por favor seleccione la sucursal' }]}
                 >
-                    <Select>
-                        <Option value="1">Sucursal Principal</Option>
-                        {/* Add more options as needed */}
-                    </Select>
+                    <Select
+                        options={branchOfficeOptions}
+                        showSearch
+                        onChange={ (value) => {
+                            // Get the selected branch office
+                            const selectedBranchOffice = branchOffices.find(office => `${office.nickname} - ${office.address}` === value);
+                            
+                            // Update shouldChargeWasteCollection in form
+                            if (selectedBranchOffice) {
+                                form.setFieldsValue({
+                                    chargeWasteCollection: selectedBranchOffice.shouldChargeWasteCollection
+                                });
+                            } else {
+                                // If there's an error or no branch office found, set to false
+                                form.setFieldsValue({
+                                    chargeWasteCollection: false
+                                });
+                            }
+                        }}
+                    />
                 </Form.Item>
+
+                <Flex wrap gap={16} align='center'>
+                    <Form.Item
+                        layout='horizontal'
+                        name="period"
+                        label="Periodo"
+                        rules={[{ required: true, message: 'Por favor ingrese el periodo' }]}
+                    >
+                        <DatePicker picker="month"/>
+                    </Form.Item>
+
+                    <Form.Item
+                        layout='horizontal'
+                        name="amount"
+                        label="Ingreso Bruto"
+                        rules={[{ required: true, message: 'Por favor ingrese el ingreso bruto' }]}
+                    >
+                        <InputNumber
+                            style={{ width: '100%' }}
+                            addonAfter='Bs'
+                            min='0'
+                            step='0.01'
+                            />
+                    </Form.Item>
+
+                        <Form.Item
+                            name="chargeWasteCollection"
+                            valuePropName="checked"
+                        >
+                            <Checkbox>¿Cobrar Aseo Urbano?</Checkbox>
+                        </Form.Item>
+                </Flex>
+
+                <Form.Item
+                    name="declaratinoImage"
+                >
+                    <Upload><Button>Subir Declaración</Button></Upload>
+                </Form.Item>
+                
+                    
 
                 <Form.Item>
                     <Button type="primary" htmlType="submit">
