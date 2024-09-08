77ca80a48b564691f8b864430ca4bcefc6e01b94
douglas571 <douglassocorro.work@gmail.com>
2024-09-08 13:12:08 -0400

    Feature: Progress adding the details view for invoice

--- frontend/src/pages/TaxCollectionBusinessGrossIncomesInvoice.tsx
+++ frontend/src/pages/TaxCollectionBusinessGrossIncomesInvoice.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Card, Typography, Table } from 'antd';
+import { Card, Typography, Table, Descriptions, List } from 'antd';
 const { Title, Text } = Typography;
 
 import dayjs from 'dayjs';
+dayjs.locale('es')
 
 const GrossIncomeInvoiceDetails: React.FC = () => {
 	// Dummy data
@@ -59,6 +60,20 @@ const GrossIncomeInvoiceDetails: React.FC = () => {
                 }
             }
 		],
+
+        createdByUser: {
+            person: {
+                firstName: 'Jose',
+                lastName: 'Herrera',
+            },
+        },
+
+        checkedByUser: {
+            person: {
+                firstName: 'Hipólita',
+                lastName: 'Gonzales',
+            },
+        },
 	};
 
     const MMVExchangeRate = Math.max(
@@ -72,9 +87,22 @@ const GrossIncomeInvoiceDetails: React.FC = () => {
         <Card title="Gross Income Invoice Details">
             <Title level={4}>Invoice #{invoiceDetails.id}</Title>
             <Text>Date: {dayjs().format('YYYY-MM-DD')}</Text>
-            <Text strong style={{ display: 'block', marginTop: 16 }}>Customer: N/A</Text>
+
+            <Title level={5} style={{ textAlign: 'center' }}>Descripción del Contribuyente</Title>
+            <Descriptions bordered layout='vertical' size='small'>
+                <Descriptions.Item label="Contribuyente" style={{ width: '20%' }} >Carnicería el poder de adonai, C.A</Descriptions.Item>
+                <Descriptions.Item label="Rif" style={{ width: '12%' }}>J-40234567-8</Descriptions.Item>
+                <Descriptions.Item label="N°" style={{ width: '12%' }}>3.2.03.01</Descriptions.Item>
+
+                <Descriptions.Item label="Ramo" style={{ width: '20%' }}>Restaurante</Descriptions.Item>
+                <Descriptions.Item label="Mts2" style={{ width: '5%' }}>50</Descriptions.Item>
+                <Descriptions.Item label="Tipo"  style={{ width: '5%' }}>I</Descriptions.Item>
+            </Descriptions>
+
+            <Title level={5} style={{ textAlign: 'center' }}>Estado de Cuenta</Title>
 
             <Table 
+                size='small'
                 dataSource={invoiceDetails.grossIncomes} 
                 pagination={false}
             >
@@ -137,6 +165,7 @@ const GrossIncomeInvoiceDetails: React.FC = () => {
             </Table>
 
             <Table 
+                size='small'
                 dataSource={[{ formularyPrice: 1 }]} 
                 pagination={false}
                 showHeader={false}
@@ -154,6 +183,7 @@ const GrossIncomeInvoiceDetails: React.FC = () => {
             </Table>
             
             <Table 
+                size='small'
                 dataSource={[{ total: 1 }]} 
                 pagination={false}
                 showHeader={false}
@@ -172,6 +202,46 @@ const GrossIncomeInvoiceDetails: React.FC = () => {
                     render={(text: any) => <Text strong>{text.total} Bs.</Text>}
                 />
             </Table>
+
+            <Table 
+                size='small'
+                dataSource={[{ total: 40 }]} 
+                pagination={false}
+                showHeader={false}
+            >
+                <Table.Column 
+                    title="Total" 
+                    key="total" 
+                    render={(text: any) => <Text strong>Total en MMV</Text>}
+                    align="right"
+                />
+                <Table.Column 
+                    title="Total en Bs" 
+                    key="total" 
+                    align="right"
+                    width="15%"
+                    render={(text: any) => <Text strong>{Number(text.total / MMVExchangeRate).toFixed(2)} MMV</Text>}
+                />
+            </Table>
+            <Typography.Paragraph style={{ textAlign: 'center', paddingTop: '10px', paddingBottom: '10px' }} strong>Tasa de cambio de la Moneda de Mayor Valor dle Banco Central de Venezuela (TCMMV)={MMVExchangeRate}Bs. desde el día 19/08/224 hasta el 23/08/2024.</Typography.Paragraph>
+
+            
+            <Descriptions bordered layout='vertical' size='small'>
+                <Descriptions.Item label="Creado por" style={{ width: '20%' }} >{invoiceDetails.createdByUser.person.firstName} {invoiceDetails.createdByUser.person.lastName}</Descriptions.Item>
+                <Descriptions.Item label="Revisado por" style={{ width: '20%' }} >{invoiceDetails.checkedByUser.person.firstName} {invoiceDetails.checkedByUser.person.lastName}</Descriptions.Item>
+            </Descriptions>
+
+            <Title level={5} style={{ textAlign: 'center' }}>Datos para Depósitos y/o Transferencias a Nombre de SEDEMAT</Title>
+            <Descriptions bordered layout='vertical' size='small'>
+                <Descriptions.Item label="Rif" span={1}>G-20012768-6</Descriptions.Item>
+                <Descriptions.Item label="Correo" span={3}>SEDEMATZAMORAFALCON.8@GMAIL.COM</Descriptions.Item>
+
+                <Descriptions.Item label="Banco de Venezuela" span={2}>Corriente 0102-0339-2500-0107-1892</Descriptions.Item>
+                <Descriptions.Item label="Banco Bicentenario" span={2}>Corriente 0175-0162-3100-7494-9290</Descriptions.Item>
+            </Descriptions>
+
+
+            
         </Card>
     );
 };
@@ -195,5 +265,15 @@ function calculateTax(grossIncome: any, MMVExchangeRate: number) {
 }
     
 
+// a function to given the whole invoice details, calculate the total in bolivarse and mmv
+function calculateTotal(invoiceDetails: any) {
+    // calculate the total in bolivarse
+    const totalBs = invoiceDetails.grossIncomes.reduce((acc: any, curr: any) => acc + curr.amountBs, 0);
 
+    // calculate the total in mmv
+    const totalMMV = totalBs / MMVExchangeRate;
+
+    return { totalBs, totalMMV };
+    
+}
 
