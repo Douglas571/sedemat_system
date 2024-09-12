dd59f578624be0de2e1098347bd661eb3de5fae0
douglas571 <douglassocorro.work@gmail.com>
2024-09-12 18:10:16 -0400

    Chores: Deleted dummy gross incomes

--- backend/database/seeders/20240908203821-adding-dummy-gross-incomes.js
+++ backend/database/seeders/20240908203821-adding-dummy-gross-incomes.js
@@ -13,30 +13,30 @@ module.exports = {
      * }], {});
     */
 
-    await queryInterface.bulkInsert('GrossIncomes', [
-      {
-        businessId: 1,
-        branchOfficeId: 1,
-        period: new Date('2024-08-01'),
-        amountBs: 5000.00,
-        chargeWasteCollection: true,
-        declarationImage: 'https://www.shutterstock.com/image-vector/image-illustration-document-order-invoice-600nw-1918048565.jpg',
-        grossIncomeInvoiceId: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        businessId: 1,
-        branchOfficeId: 1,
-        period: new Date('2024-07-01'),
-        amountBs: 6000.00,
-        chargeWasteCollection: false,
-        declarationImage: 'https://www.shutterstock.com/image-vector/image-illustration-document-order-invoice-600nw-1918048565.jpg',
-        grossIncomeInvoiceId: null,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ], {});
+    // await queryInterface.bulkInsert('GrossIncomes', [
+    //   {
+    //     businessId: 1,
+    //     branchOfficeId: 1,
+    //     period: new Date('2024-08-01'),
+    //     amountBs: 5000.00,
+    //     chargeWasteCollection: true,
+    //     declarationImage: 'https://www.shutterstock.com/image-vector/image-illustration-document-order-invoice-600nw-1918048565.jpg',
+    //     grossIncomeInvoiceId: null,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     businessId: 1,
+    //     branchOfficeId: 1,
+    //     period: new Date('2024-07-01'),
+    //     amountBs: 6000.00,
+    //     chargeWasteCollection: false,
+    //     declarationImage: 'https://www.shutterstock.com/image-vector/image-illustration-document-order-invoice-600nw-1918048565.jpg',
+    //     grossIncomeInvoiceId: null,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   }
+    // ], {});
   },
 
   async down (queryInterface, Sequelize) {
