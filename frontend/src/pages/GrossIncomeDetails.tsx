bc0e3891b64d8c3c0da76f8df263b3a25c36c947
douglas571 <douglassocorro.work@gmail.com>
2024-10-09 11:05:19 -0400

    Feature: Updated the fields

--- frontend/src/pages/GrossIncomeDetails.tsx
+++ frontend/src/pages/GrossIncomeDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 
 import { Card, Typography, Button, Flex, Descriptions, Image, Popconfirm, message } from 'antd';
 
@@ -8,7 +8,8 @@ import * as api from '../util/api';
 
 import { IGrossIncome, Business } from '../util/types';
 import { completeUrl } from '../util';
-import { formatBolivares } from '../util/currency';
+import { formatBolivares, formatPercents } from '../util/currency';
+import dayjs from 'dayjs';
 
 const { Title, Text } = Typography;
 
@@ -17,7 +18,7 @@ const GrossIncomeDetails: React.FC = () => {
     const [grossIncome, setGrossIncome] = React.useState<IGrossIncome>();
     const [business, setBusiness] = React.useState<Business>();
     const navigate = useNavigate();
-    
+
     useEffect(() => {
         loadGrossIncome();
         loadBusiness();
@@ -50,7 +51,7 @@ const GrossIncomeDetails: React.FC = () => {
 
     return (
         <Card title={(
-            <Flex justify='space-between' align='center'>
+            <Flex justify='space-between' align='center' gap={16}>
                 <Title level={2}>Ingresos Brutos Declarados</Title>
                 <Flex gap={10}>
                     <Button onClick={() => navigate(`/tax-collection/${businessId}/gross-incomes/${grossIncomeId}/edit`)}>Editar</Button>
@@ -61,16 +62,20 @@ const GrossIncomeDetails: React.FC = () => {
                         cancelText="No"
                     >
                         <Button danger>Eliminar</Button>
+
                     </Popconfirm>
+                    {grossIncome.grossIncomeInvoiceId && (<Button>
+                        <Link to={`/tax-collection/${businessId}/gross-incomes-invoice/${grossIncome.grossIncomeInvoiceId}`}>Factura</Link>
+                    </Button>)}
+
                 </Flex>
             </Flex>
         )}>
 
-            <BusinessInfo 
-                business={business} 
-                alicuotaTaxPercent={`${grossIncome.alicuota.taxPercent * 100}%`}
-                minTaxMMV={`${grossIncome.alicuota.minTaxMMV} MMV-BCV`}
-                TCMMVBCV={formatBolivares(grossIncome.TCMMVBCV)}
+            <BusinessInfo
+                business={business}
+                grossIncome={grossIncome}
+                economicActivityTitle={business?.economicActivity?.title}
 
             />
 
@@ -80,22 +85,23 @@ const GrossIncomeDetails: React.FC = () => {
     );
 };
 
-const BusinessInfo: React.FC<{ 
-    business: Business | undefined 
-    alicuotaTaxPercent: string | undefined 
-    minTaxMMV: string | undefined,
-    TCMMVBCV: string | undefined,
-}> = ({ business, alicuotaTaxPercent, minTaxMMV, TCMMVBCV }) => {
-    if (!business) return null;
+const BusinessInfo: React.FC<{
+    business: Business | undefined,
+    grossIncome: IGrossIncome | undefined,
+    economicActivityTitle: string | undefined
+}> = ({ business, grossIncome, economicActivityTitle }) => {
+
+    if (!grossIncome || !business) return null;
+
 
     return (
         <Descriptions title="Información del Negocio" bordered>
             <Descriptions.Item label="Nombre del Negocio">{business.businessName}</Descriptions.Item>
             <Descriptions.Item label="RIF">{business.dni}</Descriptions.Item>
-            <Descriptions.Item label="Actividad Económica">{business.economicActivity?.title}</Descriptions.Item>
-            <Descriptions.Item label="Alicuota">{alicuotaTaxPercent}</Descriptions.Item>
-            <Descriptions.Item label="Mínimo Tributario">{minTaxMMV}</Descriptions.Item>
-            <Descriptions.Item label="TC-MMVBCV">{TCMMVBCV}</Descriptions.Item>
+            <Descriptions.Item label="Actividad Económica">{economicActivityTitle}</Descriptions.Item>
+            <Descriptions.Item label="Alicuota">{formatPercents(grossIncome.alicuotaTaxPercent)}</Descriptions.Item>
+            <Descriptions.Item label="Mínimo Tributario">{grossIncome.alicuotaMinTaxMMVBCV} MMVBCV</Descriptions.Item>
+            <Descriptions.Item label="TC-MMVBCV">{formatBolivares(grossIncome.TCMMVBCV)}</Descriptions.Item>
         </Descriptions>
     );
 };
@@ -106,10 +112,10 @@ const GrossIncomeInfo: React.FC<{ grossIncome: IGrossIncome | undefined }> = ({ 
 
     return (
         <Descriptions title="Detalles del Ingreso Bruto" bordered>
-            <Descriptions.Item label="Año">{grossIncome.period.year()}</Descriptions.Item>
-            <Descriptions.Item label="Mes">{grossIncome.period.format('MMMM')}</Descriptions.Item>
-            <Descriptions.Item label="Monto Declarado">{`${grossIncome.amountBs} Bs.`}</Descriptions.Item>
-            <Descriptions.Item label="Cobrar Aseo">{ grossIncome.wasteCollectionTaxId ? "SI" : "NO" }</Descriptions.Item>
+            <Descriptions.Item label="Año">{dayjs(grossIncome.period).year()}</Descriptions.Item>
+            <Descriptions.Item label="Mes">{dayjs(grossIncome.period).format('MMMM')}</Descriptions.Item>
+            <Descriptions.Item label="Monto Declarado">{formatBolivares(grossIncome.amountBs)}</Descriptions.Item>
+            <Descriptions.Item label="Cobrar Aseo">{grossIncome.chargeWasteCollection ? "SI" : "NO"}</Descriptions.Item>
             <Descriptions.Item label="Sucursal">{grossIncome.branchOffice.nickname}</Descriptions.Item>
         </Descriptions>
     );
