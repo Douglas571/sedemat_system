7e138a7db2b3cc60e76944f8bbc7c7a35ef9ebaf
douglas571 <douglassocorro.work@gmail.com>
2024-10-25 11:08:02 -0400

    Feature: Added report in case no branch office

--- backend/services/reports/businessReportsService.js
+++ backend/services/reports/businessReportsService.js
@@ -169,7 +169,6 @@ function getBusinessesGrossIncomeReport(businesses) {
             } else {
               // branch office lack declaration for this period
               branchOfficeReport.monthsWithoutDeclarationCount += 1
-              branchOfficeReport.monthsPendingToBePaidCount += 1
 
               // if there is a gross income for this period
               if (grossIncome) {
@@ -183,24 +182,83 @@ function getBusinessesGrossIncomeReport(businesses) {
           initialYear++
         }
 
-        // get the gross incomes that lack the declaration 
-          // iterate from each months since initial period 
-            // if month 
-
-
-        // count how many gross incomes are pending or lacking a declaration
-
-        // count how many gross incomes are pending to be settled 
-        
         businessReport.branchOffices.push(branchOfficeReport)
       })
-    }
+    } else {
+      // if it not has branch office
+      businessReport = {
+        ...businessReport,
+        monthsWithoutDeclarationCount: 0,
+        monthsPendingToBePaidCount: 0,
+        monthsPendingToBeSettledCount: 0,
+        monthsSettledCount: 0,
 
-    // if it not has branch office 
+        monthsWithoutDeclaration: [],
+        monthsPendingToBePaid: [],
+        monthsPendingToBeSettled: [],
+        monthsSettled: [],
+      }
+
+      businessReport.lastMonthSettled = INITIAL_DATE
+
+      let lastPeriodSettled = business.grossIncomes.filter( g => g?.grossIncomeInvoice?.settlement != null ).sort((a, b) => dayjs(a.period) - dayjs(b.period)).pop()?.period
+
+      if (lastPeriodSettled) {
+        businessReport.lastMonthSettled = dayjs(lastPeriodSettled)
+      }
+
+      let initialYear = businessReport.lastMonthSettled.year()
+      let finalYear = CURRENT_DATE.year()
+
+      while (initialYear <= finalYear) {
+
+        let initialMonth = businessReport.lastMonthSettled.month()
+        let finalMonth = CURRENT_DATE.month()
+
+        while (initialMonth < finalMonth) {
+
+          let grossIncome = business.grossIncomes.find( g => dayjs(g.period).year() === initialYear && dayjs(g.period).month() === initialMonth)
+
+          if (grossIncome && grossIncome.declarationImage) {
+
+            // check if it's pending to be paid 
+            if (!grossIncome?.grossIncomeInvoice?.paidAt) {
+              businessReport.monthsPendingToBePaidCount += 1
+              businessReport.monthsPendingToBePaid.push(grossIncome)
+            }
+
+            // check if it's pending to be settled
+            if (!grossIncome?.grossIncomeInvoice?.settlement && grossIncome?.grossIncomeInvoice?.paidAt) { 
+              businessReport.monthsPendingToBeSettledCount += 1
+              businessReport.monthsPendingToBeSettled.push(grossIncome)
+            }
+
+            // check if it's settled
+            if (grossIncome?.grossIncomeInvoice?.settlement) {
+              // keep this just in case, we need to clarify since when start counting settled months
+              businessReport.monthsSettledCount += 1
+              businessReport.monthsSettled.push(grossIncome)
+            }
+
+          } else {
+            // branch office lack declaration for this period
+            businessReport.monthsWithoutDeclarationCount += 1
+
+            // if there is a gross income for this period
+            if (grossIncome) {
+              businessReport.monthsWithoutDeclaration.push(grossIncome)
+            }
+          }
+          initialMonth++
+        }
+
+        initialYear++
+      }
+
+    }
 
     report.push(businessReport)
   })
-  
-  return report
 
+  return report
 }
