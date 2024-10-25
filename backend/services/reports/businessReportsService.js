2a875bcde8358b56f05080da919cae577fd366f3
douglas571 <douglassocorro.work@gmail.com>
2024-10-25 15:26:37 -0400

    Refactoring: Simplify the code

--- backend/services/reports/businessReportsService.js
+++ backend/services/reports/businessReportsService.js
@@ -146,9 +146,9 @@ function mapBusinessToRowReport(businessReport){
 function getBusinessesGrossIncomeReport(businesses) {
   const CURRENT_DATE = dayjs();
 
-  console.log({INITIAL_DATE, CURRENT_DATE});
-
   let report = []
+
+  let reportRows = []
 
   // iterate over each branch office 
   businesses.map((business) => {
@@ -166,25 +166,11 @@ function getBusinessesGrossIncomeReport(businesses) {
       // for each branch office
       business.branchOffices.map((branchOffice) => {
         
-        let branchOfficeReport = {
-          ...branchOffice,
-          monthsWithoutDeclarationCount: 0,
-          monthsPendingToBePaidCount: 0,
-          monthsPendingToBeSettledCount: 0,
-          monthsSettledCount: 0,
-
-          monthsWithoutDeclaration: [],
-          monthsPendingToBePaid: [],
-          monthsPendingToBeSettled: [],
-          monthsSettled: [],
-        }
-
+        let branchOfficeReport = {}
+        branchOfficeReport.lastMonthSettled = INITIAL_DATE
         
         // get the gross incomes for that branch office
-        branchOfficeReport.grossIncomes = business.grossIncomes.filter( g => g.branchOfficeId === branchOffice.id)
-
-        // get the initial gross income period to count 
-        branchOfficeReport.lastMonthSettled = INITIAL_DATE
+        branchOfficeReport.grossIncomes = business.grossIncomes.filter( g => g.branchOfficeId === branchOffice.id)        
 
         // find the last month settled
         let lastPeriodSettled = branchOfficeReport.grossIncomes.filter( g => g?.grossIncomeInvoice?.settlement != null ).sort((a, b) => dayjs(a.period) - dayjs(b.period)).pop()?.period
@@ -193,76 +179,19 @@ function getBusinessesGrossIncomeReport(businesses) {
           branchOfficeReport.lastMonthSettled = dayjs(lastPeriodSettled)
         }
 
-        console.log({lastPeriodSettled: dayjs(lastPeriodSettled)})
 
-        let initialYear = branchOfficeReport.lastMonthSettled.year()
-        let finalYear = CURRENT_DATE.year()
+        branchOfficeReport = {
+          ...businessReport,
+          ...branchOffice,
+          ...branchOfficeReport,
+          ...getGrossIncomeReport({
+            lastMonthSettled: branchOfficeReport.lastMonthSettled, grossIncomes: branchOfficeReport.grossIncomes}),
 
-        while (initialYear <= finalYear) {
-
-          // console.log({initialYear, finalYear})
-
-          let initialMonth = branchOfficeReport.lastMonthSettled.month()
-          let finalMonth = CURRENT_DATE.month()
-
-          while (initialMonth < finalMonth) {
-            // console.log({initialMonth, finalMonth})
-
-            let grossIncome = branchOfficeReport.grossIncomes.find( g => dayjs(g.period).year() === initialYear && dayjs(g.period).month() === initialMonth)
-
-            if (grossIncome && grossIncome.declarationImage) {
-
-              // check if it's pending to be paid 
-              if (!grossIncome?.grossIncomeInvoice?.paidAt) {
-                branchOfficeReport.monthsPendingToBePaidCount += 1
-                branchOfficeReport.monthsPendingToBePaid.push(grossIncome)
-              }
-
-              // check if it's pending to be settled
-              if (!grossIncome?.grossIncomeInvoice?.settlement && grossIncome?.grossIncomeInvoice?.paidAt) { 
-                branchOfficeReport.monthsPendingToBeSettledCount += 1
-                branchOfficeReport.monthsPendingToBeSettled.push(grossIncome)
-              }
-
-              // check if it's settled
-              if (grossIncome?.grossIncomeInvoice?.settlement) {
-                // keep this just in case, we need to clarify since when start counting settled months
-                branchOfficeReport.monthsSettledCount += 1
-                branchOfficeReport.monthsSettled.push(grossIncome)
-              }
-
-            } else {
-              // branch office lack declaration for this period
-              branchOfficeReport.monthsWithoutDeclarationCount += 1
-
-              // if there is a gross income for this period
-              if (grossIncome) {
-                branchOfficeReport.monthsWithoutDeclaration.push(grossIncome)
-              }
-            }
-
-            initialMonth++
-          }
-
-          initialYear++
         }
-
         businessReport.branchOffices.push(branchOfficeReport)
+
       })
     } else { // if it not has branch office
-      
-      businessReport = {
-        ...businessReport,
-        monthsWithoutDeclarationCount: 0,
-        monthsPendingToBePaidCount: 0,
-        monthsPendingToBeSettledCount: 0,
-        monthsSettledCount: 0,
-
-        monthsWithoutDeclaration: [],
-        monthsPendingToBePaid: [],
-        monthsPendingToBeSettled: [],
-        monthsSettled: [],
-      }
 
       businessReport.lastMonthSettled = INITIAL_DATE
 
@@ -272,52 +201,12 @@ function getBusinessesGrossIncomeReport(businesses) {
         businessReport.lastMonthSettled = dayjs(lastPeriodSettled)
       }
 
-      let initialYear = businessReport.lastMonthSettled.year()
-      let finalYear = CURRENT_DATE.year()
-
-      while (initialYear <= finalYear) {
-
-        let initialMonth = businessReport.lastMonthSettled.month()
-        let finalMonth = CURRENT_DATE.month()
-
-        while (initialMonth < finalMonth) {
-
-          let grossIncome = business.grossIncomes.find( g => dayjs(g.period).year() === initialYear && dayjs(g.period).month() === initialMonth)
-
-          if (grossIncome && grossIncome.declarationImage) {
-
-            // check if it's pending to be paid 
-            if (!grossIncome?.grossIncomeInvoice?.paidAt) {
-              businessReport.monthsPendingToBePaidCount += 1
-              businessReport.monthsPendingToBePaid.push(grossIncome)
-            }
-
-            // check if it's pending to be settled
-            if (!grossIncome?.grossIncomeInvoice?.settlement && grossIncome?.grossIncomeInvoice?.paidAt) { 
-              businessReport.monthsPendingToBeSettledCount += 1
-              businessReport.monthsPendingToBeSettled.push(grossIncome)
-            }
-
-            // check if it's settled
-            if (grossIncome?.grossIncomeInvoice?.settlement) {
-              // keep this just in case, we need to clarify since when start counting settled months
-              businessReport.monthsSettledCount += 1
-              businessReport.monthsSettled.push(grossIncome)
-            }
-
-          } else {
-            // branch office lack declaration for this period
-            businessReport.monthsWithoutDeclarationCount += 1
-
-            // if there is a gross income for this period
-            if (grossIncome) {
-              businessReport.monthsWithoutDeclaration.push(grossIncome)
-            }
-          }
-          initialMonth++
-        }
-
-        initialYear++
+      businessReport = {
+        ...business,
+        ...businessReport,
+        ...getGrossIncomeReport({
+          lastMonthSettled: businessReport.lastMonthSettled, 
+          grossIncomes: business.grossIncomes})
       }
 
     }
@@ -331,11 +220,6 @@ function getBusinessesGrossIncomeReport(businesses) {
 
 // TODO: In progress...
 function getGrossIncomeReport({
-  // business, branchOffice, grossIncomes, lastMonthSettled
-
-  // grossIncomes
-  // lastPeriodPaid date
-
   lastMonthSettled,
   grossIncomes
 
@@ -353,6 +237,8 @@ function getGrossIncomeReport({
     monthsSettled: []
   }
 
+  const CURRENT_DATE = dayjs();
+
   let initialYear = lastMonthSettled.year()
   let finalYear = CURRENT_DATE.year()
 
@@ -366,7 +252,7 @@ function getGrossIncomeReport({
     while (initialMonth < finalMonth) {
       // console.log({initialMonth, finalMonth})
 
-      let grossIncome = report.grossIncomes.find( g => dayjs(g.period).year() === initialYear && dayjs(g.period).month() === initialMonth)
+      let grossIncome = grossIncomes.find( g => dayjs(g.period).year() === initialYear && dayjs(g.period).month() === initialMonth)
 
       if (grossIncome && grossIncome.declarationImage) {
 
