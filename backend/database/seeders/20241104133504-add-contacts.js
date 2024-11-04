3911ff7cfa8aed68baed165a23370406452514c9
douglas571 <douglassocorro.work@gmail.com>
2024-11-04 18:41:39 -0400

    Feature: Added a contacts seeder

--- backend/database/seeders/20241104133504-add-contacts.js
+++ backend/database/seeders/20241104133504-add-contacts.js
@@ -2,26 +2,31 @@
 
 const fs = require('fs');
 const csv = require('csv-parser');
+const path = require('path');
 
 /** @type {import('sequelize-cli').Migration} */
 
 async function readCsvFile(pathToContactsCsv) {
-  const rows = [];
-  fs.createReadStream(pathToContactsCsv)
-    .pipe(csv())
-    .on('data', (row) => {
-      rows.push(row);
-    })
-    .on('end', () => {
-      return rows;
-    });
+  return new Promise((resolve, reject) => {
+    const rows = [];
+    fs.createReadStream(pathToContactsCsv)
+      .pipe(csv())
+      .on('data', (row) => {
+        rows.push(row);
+      })
+      .on('end', () => {
+        resolve(rows);
+      })
+      .on('error', reject);
+  });
 }
 
 module.exports = {
   async up (queryInterface, Sequelize) {
     let contacts = [];
 
-    const rows = await readCsvFile('contacts.csv');
+    const rows = await readCsvFile(path.resolve(__dirname, 'contacts.csv'));
+    // console.log({rows});
 
     // Iterate over all contact rows
     rows.forEach((row) => {
@@ -30,22 +35,26 @@ module.exports = {
       // Extract owner contact
       const ownerContact = {
         id: row.ownerId,
-        fullName: row.ownerFullName,
+        // fullName: row.ownerFullName,
+        firstName: row.ownerFullName, // row.ownerFullName?.split(' ')[0] || '',
+        lastName: '', // row.ownerFullName?.split(' ')[1] || '',
         dni: row.ownerDni,
         phone: row.ownerPhone,
         email: row.ownerEmail,
         isOwner: true,
-        businessId: [currentBusinessId],
+        businessesIds: [Number(currentBusinessId)],
       };
 
       // Extract accountant contact
       const accountantContact = {
         id: row.accountantId,
-        fullName: row.accountattFullName,
+        // fullName: row.accountantFullName,
+        firstName: row.accountantFullName, // row.accountantFullName?.split(' ')[0] || '',
+        lastName: '', // row.accountantFullName?.split(' ')[1] || '',
         dni: row.accountantDni,
         phone: row.accountantPhone,
         isOwner: false,
-        businessId: [currentBusinessId],
+        businessesIds: [Number(currentBusinessId)],
       };
 
       // Create variables to track new owners and accountants
@@ -54,27 +63,54 @@ module.exports = {
 
       // Map existing contacts array
       contacts = contacts.map((currentContact) => {
+        // console.log({currentContact});
         if (currentContact.id === row.ownerId) {
-          currentContact.businessesIds.push(currentBusinessId);
+          currentContact.businessesIds.push(Number(currentBusinessId));
           isANewOwner = false;
         }
         if (currentContact.id === row.accountantId) {
-          currentContact.businessesIds.push(currentBusinessId);
+          currentContact.businessesIds.push(Number(currentBusinessId));
           isANewAccountant = false;
         }
         return currentContact;
       });
 
       // Add new owner and accountant to contacts array if necessary
-      if (isANewOwner) {
+      if (isANewOwner && ownerContact.id) {
         contacts.push(ownerContact);
       }
-      if (isANewAccountant) {
+      if (isANewAccountant && accountantContact.id) {
         contacts.push(accountantContact);
       }
     });
 
-    console.log({contacts});
+    // console.log({contacts});
+
+    let contactsFormatted = contacts.map(contact => {
+      let newObject = { ...contact, isOwner: undefined, createdAt: new Date(), updatedAt: new Date() }
+
+      delete newObject.isOwner;
+      delete newObject.businessesIds;
+
+      return newObject
+    })
+
+    // console.log({contactsFormatted})
+
+    await queryInterface.sequelize.transaction(async (transaction) => {
+
+      await queryInterface.bulkInsert('People', contactsFormatted, { transaction });
+
+      for (const contact of contacts) {
+        await queryInterface.bulkUpdate('Businesses', {
+          ownerPersonId: contact.isOwner ? contact.id : null,
+          accountantPersonId: !contact.isOwner ? contact.id : null
+        }, {
+          id: contact.businessesIds
+        }, { transaction })
+      }
+
+    })
 
     // await queryInterface.bulkInsert('Contacts', contacts, {});
   },
@@ -86,5 +122,28 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+
+    const rows = await readCsvFile(path.resolve(__dirname, 'contacts.csv'));
+
+    let businessesIds = []
+    let ownerPersonId = []
+    let accountantPersonId = []
+
+    rows.forEach((row) => {
+      businessesIds.push(row.businessId)
+      ownerPersonId.push(row.ownerId)
+      accountantPersonId.push(row.accountantId)
+    })
+
+    await queryInterface.bulkUpdate('Businesses', {
+      ownerPersonId: null,
+      accountantPersonId: null
+    }, {
+      id: businessesIds
+    })
+
+    await queryInterface.bulkDelete('People', {
+      id: [...ownerPersonId, ...accountantPersonId]
+    })
   }
 };
