e2bdb9ab001aec5ac81ae107385516bb5c29e527
douglas571 <douglassocorro.work@gmail.com>
2024-11-30 22:55:42 -0400

    Revert "Style: Put the rif first in names"
    This reverts commit fe750a099595de11e637020c0ea19da9624d9973.

--- frontend/src/pages/BusinessesViewByEconomicActivityIndex.tsx
+++ frontend/src/pages/BusinessesViewByEconomicActivityIndex.tsx
@@ -42,7 +42,7 @@ const BusinessesViewByEconomicActivityIndex: React.FC = () => {
 
     if (treeNode.businessName) {
       return {
-        title: <Link to={`/business/${treeNode.id}`}>{treeNode.dni} - {treeNode.businessName} </Link>,
+        title: <Link to={`/business/${treeNode.id}`}>{treeNode.businessName} - {treeNode.dni} </Link>,
         value: treeNode.businessName,
         key: treeNode.dni,
       }
