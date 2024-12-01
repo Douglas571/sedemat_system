0ce84f774a4f9ef6f1cbb218694a47c6e6eb52a3
douglas571 <douglassocorro.work@gmail.com>
2024-11-30 21:43:20 -0400

    Feature: Improved frontend code to create business index tree by economic activity

--- frontend/src/pages/BusinessesViewByEconomicActivityIndex.tsx
+++ frontend/src/pages/BusinessesViewByEconomicActivityIndex.tsx
@@ -17,7 +17,6 @@ const BusinessesViewByEconomicActivityIndex: React.FC = () => {
   const loadIndex = async () => {
     let index = await getBusinessEconomicActivityIndex(userAuth.token)
 
-    console.log({index})
     setEconomicActivityIndex(index)
   }
 
@@ -25,100 +24,40 @@ const BusinessesViewByEconomicActivityIndex: React.FC = () => {
     loadIndex()
   }, [])
 
-  const mappedTreeData: TreeDataNode[] = economicActivityIndex?.map((sector) => {
-
+  function mapEconomicActivityBusinessIndex(treeNode) {      
     let children = []
-    let businessCount0 = 0
+    let businessCount = 0
 
-    children =sector.economicActivities.map((economicActivity) => {
-
-      let children = []
-      let businessCount1 = 0
-      
-      if (economicActivity.economicActivities) {
-        children = economicActivity.economicActivities.map((activity) => {
-          let children = []
-
-          let businessCount2 = 0
-
-          if (activity.economicActivities) {
-            
-
-            children = activity.economicActivities.map((subActivity) => {
-              let businessCount3 = 0
-              businessCount3 = subActivity?.businesses?.length
-
-              businessCount2 += businessCount3
-              
-              // businessCount2 += businessCount3
-
-              return {
-                title: <Typography.Text style={{ fontWeight: businessCount3 > 0 ? 'bold' : 'normal'}}>{`${subActivity.code} - ${subActivity.title} (${businessCount3})`}</Typography.Text>,
-                value: subActivity.code,
-                key: subActivity.code,
-                children: subActivity?.businesses?.map((business) => {
-                  return {
-                    title: <Link to={`/business/${business.id}`}>{business.businessName} - {business.dni} </Link>,
-                    value: business.dni,
-                    key: business.dni,
-                  }
-                })
-              }
-            })
-
-            businessCount1 += businessCount2
-          } else {
-            children = activity?.businesses?.map((business) => {
-              return {
-                title: <Link to={`/business/${business.id}`}>{business.businessName} - {business.dni} </Link>,
-                value: business.dni,
-                key: business.dni,
-              }
-            })
-
-            businessCount2 += activity.businesses.length
-            businessCount1 += businessCount2
-          }
-
-          return {
-            title: <Typography.Text style={{ fontWeight: businessCount2 > 0 ? 'bold' : 'normal'}}>{`${activity.code} - ${activity.title} (${businessCount2})`}</Typography.Text>,
-            value: activity.code,
-            key: activity.code,
-            children,
-          }
-        })
-      } else {
-        children = economicActivity.businesses.map((business) => {
-          return {
-            title: <Link to={`/business/${business.id}`}>{business.businessName} - {business.dni} </Link>,
-            value: business.dni,
-            key: business.dni,
-          }
-        })
-
-        businessCount1 = economicActivity.businesses.length
-      }
-
-      businessCount0 += businessCount1
-
-      return {
-        title: <Typography.Text style={{ fontWeight: businessCount1 > 0 ? 'bold' : 'normal'}}>{`${economicActivity.code} - ${economicActivity.title} (${businessCount1})`}</Typography.Text>,
-        value: economicActivity.code,
-        key: economicActivity.code,
-        children: children
-      }
-    })
-
-    let formattedSector = {
-      title: <Typography.Text style={{ fontWeight: businessCount0 > 0 ? 'bold' : 'normal'}}>{`${sector.code} - ${sector.title} (${businessCount0})`}</Typography.Text>,
-      value: sector.code,
-      key: sector.code,
-
-      children
+    // if it has economic activities 
+    if (treeNode.economicActivities) {
+      children = treeNode.economicActivities.map((activity) => mapEconomicActivityBusinessIndex(activity)).sort((a, b) => a.value.localeCompare(b.value))
+      businessCount = children.reduce((sum, curr) => sum + curr.businessCount, 0)
     }
 
-    return formattedSector
-  })
+    // if it don't have business or economicActivities 
+    if (treeNode.businesses) {
+      children = treeNode.businesses.map((activity) => mapEconomicActivityBusinessIndex(activity)).sort((a, b) => a.value.localeCompare(b.value))
+      businessCount = treeNode.businesses.length
+    }
+
+    if (treeNode.businessName) {
+      return {
+        title: <Link to={`/business/${treeNode.id}`}>{treeNode.businessName} - {treeNode.dni} </Link>,
+        value: treeNode.businessName,
+        key: treeNode.dni,
+      }
+    }
+
+    return {
+      title: <Typography.Text style={{ fontWeight: businessCount > 0 ? 'bold' : 'normal'}}>{`${treeNode.code} - ${treeNode.title} (${businessCount})`}</Typography.Text>,
+      value: treeNode.code,
+      key: treeNode.code,
+      children,
+      businessCount
+    }
+  }
+
+  const mappedTreeData: TreeDataNode[] = economicActivityIndex?.map((sector) => mapEconomicActivityBusinessIndex(sector))
 
   return <Card>
     <Typography.Title level={1}>Indice de empresas por actividad economica</Typography.Title>
