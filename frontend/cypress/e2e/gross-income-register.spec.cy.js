bc99a5b3464c3cfcac9ef954f00bf861187d65c5
Douglas Socorro <juancarlossocorro571@gmail.com>
2025-02-16 17:06:12 -0400

    refact: improve testing

--- frontend/cypress/e2e/gross-income-register.spec.cy.js
+++ frontend/cypress/e2e/gross-income-register.spec.cy.js
@@ -42,7 +42,12 @@ function selectDropdown(testId, optionText) {
 Cypress.Commands.add('selectDropdown', selectDropdown)
 
 
-function deleteDeclaration(period) {
+function deleteDeclaration({businessId, period}) {
+
+  cy.visit(`${BASE_URL}/tax-collection/${businessId}`)
+  cy.get(`[data-test-id="declarations-tab"]`).click()
+
+
   cy.get(`[data-test-id="declarations-tab"]`).click()
   cy.get(`[data-test-id='delete-gross-income-button-${period}']`).click()
 
@@ -57,12 +62,11 @@ function createGrossIncome({
   override
 }) {
   cy.visit(`${BASE_URL}/tax-collection/${businessId}`)
-
   cy.get(`[data-test-id="declarations-tab"]`).click()
 
   cy.get('body').then(($body) => {
     if ($body.find(`[data-test-id='delete-gross-income-button-${grossIncome.period}']`).length && override) {
-      deleteDeclaration(grossIncome.period)
+      deleteDeclaration({businessId, period: grossIncome.period})
     }
 
     cy.log("the element exists, about to delete")
@@ -98,21 +102,30 @@ function createGrossIncome({
 
     // delete the gross income 
     if (deleteAfter) {
-      deleteDeclaration(grossIncome.period)
+      deleteDeclaration({
+        businessId, period: grossIncome.period
+      })
     }
     
   })
 }
 
 function deleteInvoice({
+  businessId,
   period,
 }) {
+
+  cy.visit(`${BASE_URL}/tax-collection/${businessId}`)
+  cy.get(`[data-test-id="invoices-tab"]`).click()
   
   cy.get(`[data-test-id="invoices-tab"]`).click()
-  cy.get(`[data-test-id='delete-gross-income-button'][data-test-months*="${period}"]`).click()
-
-  cy.get('.ant-popconfirm-buttons').contains('Sí').click()
-  cy.get(`[data-test-id='delete-gross-income-button'][data-test-months*="${period}"]`).should('not.exist');
+  cy.get(`[data-test-id='delete-gross-income-button'][data-test-months*="${period}"]`).then(($el) => {
+    if ($el) {
+      cy.wrap($el).click()
+      cy.get('.ant-popconfirm-buttons').contains('Sí').click()
+      cy.get(`[data-test-id='delete-gross-income-button'][data-test-months*="${period}"]`).should('not.exist');
+    }
+  })
 }
 
 function createInvoice({
@@ -120,62 +133,75 @@ function createInvoice({
   grossIncomes,
   deleteAfter,
   override
-}) {
+}) {  
+
+
   cy.visit(`${BASE_URL}/tax-collection/${businessId}`)
   cy.get(`[data-test-id="invoices-tab"]`).click()
-
-  cy.get('body').then(($body) => {
-
-
-    cy.log(grossIncomes[0].period)
     
+  // grossIncomes.forEach( g => {
+
+  //   cy.get('body').then(($body) => {
+  //     if ($body.find(`[data-test-months*="${g.period}"]`).length && override) {
+  //       return true
+  //     } 
+  //     return false
+  //   }).then( shouldDelete => {
+  //     if (shouldDelete) {
+  //       deleteInvoice({period: g.period})
+  //     }
+  //   })
+
+  // })
+  
+  // else if ($body.find(`[data-test-months*="${grossIncomes[0].period}"]`).length && !override) {
+  //   return 
+  // }
+
+  cy.get(`[data-test-id='gross-income-invoice-add-button']`).click()
+
+  cy.wait(1000)
+
+
+  cy.get(`[data-test-id='select-created-by-user-id']`).click()
+  cy.get(`.ant-select-item-option-content`).contains('pola').click()   
+
+  cy.get(`[date-test-id='update-created-by-user-button']`).click()
+  cy.get(`[date-test-id='update-checked-by-user-button']`).click()
+
+  // Modify the valid currency exchange rate range
+  let week = dayjs().week(); // Get the current week number
+  cy.get(`input#TCMMVBCVValidDateRange`).clear().type(`2025-${week}º`); // Type the week in the format "2025-<week>º"
+
+  // Modify the issuedAt date to be in the valid range
+  let monday = dayjs().startOf('week').add(1, 'day').format('YYYY-MM-DD'); // Get the Monday of the current week
+  cy.get(`input#issuedAt`).clear().type(monday); // Type the date in the format YYYY-MM-DD
+
+  // Click the body at coordinates x=50, y=50 to close any open dropdowns or modals
+  cy.get('body').click(50, 50);
+
+  // Click the checkbox
+  cy.get('[aria-label="Select all"]').click();
+
+  cy.get(`[data-test-id='submit-form-button']`).click()
+
+  cy.get('.ant-message-notice-content', { timeout: 10000 }).should('be.visible')
+
+  if (deleteAfter) {
     grossIncomes.forEach( g => {
+
+    cy.get('body').then(($body) => {
       if ($body.find(`[data-test-months*="${g.period}"]`).length && override) {
-        deleteInvoice({period: g.period})
+        return true
       } 
-    })
-    
-    // else if ($body.find(`[data-test-months*="${grossIncomes[0].period}"]`).length && !override) {
-    //   return 
-    // }
-
-    cy.get(`[data-test-id='gross-income-invoice-add-button']`).click()
-
-    cy.wait(1000)
-
-
-    cy.get(`[data-test-id='select-created-by-user-id']`).click()
-    cy.get(`.ant-select-item-option-content`).contains('pola').click()   
-
-    cy.get(`[date-test-id='update-created-by-user-button']`).click()
-    cy.get(`[date-test-id='update-checked-by-user-button']`).click()
-
-    // Modify the valid currency exchange rate range
-    let week = dayjs().week(); // Get the current week number
-    cy.get(`input#TCMMVBCVValidDateRange`).clear().type(`2025-${week}º`); // Type the week in the format "2025-<week>º"
-
-    // Modify the issuedAt date to be in the valid range
-    let monday = dayjs().startOf('week').add(1, 'day').format('YYYY-MM-DD'); // Get the Monday of the current week
-    cy.get(`input#issuedAt`).clear().type(monday); // Type the date in the format YYYY-MM-DD
-
-    // Click the body at coordinates x=50, y=50 to close any open dropdowns or modals
-    cy.get('body').click(50, 50);
-
-    // Click the checkbox
-    cy.get('[aria-label="Select all"]').click();
-
-    cy.get(`[data-test-id='submit-form-button']`).click()
-
-    cy.get('.ant-message-notice-content', { timeout: 10000 }).should('be.visible')
-
-    if (deleteAfter) {
-      grossIncomes.forEach( g => {
-        if ($body.find(`[data-test-months*="${g.period}"]`).length) {
-          deleteInvoice({period: g.period})
-        } 
-      })
-    }
+      return false
+    }).then( shouldDelete => {
+      if (shouldDelete) {
+        deleteInvoice({businessId, period: g.period})
+      }
+    })    
   })
+  }
 }
 
 describe('Settlement printable page', () => {
@@ -211,32 +237,7 @@ describe('Settlement printable page', () => {
 
   })
 
-  it.skip('tax collector should be able to register 2 gross incomes', () => {
-
-    let businessId = 1
-
-    let grossIncomes = [
-      {
-        period: '2024-10'
-      },
-      {
-        period: '2024-11'
-      }
-    ]
-
-    grossIncomes.forEach( g => {
-      createGrossIncome({
-        businessId,
-        grossIncome: g,
-        override: true,
-        deleteAfter: false
-      })
-    })
-
-
-  })
-
-  it('tax collector should be able to create an invoice', () => {
+  it('tax collector should be able to register 2 gross incomes and create an invoice', () => {
 
     // cy.get('[data-test="total-bs"]').should('be.visible')
 
@@ -252,14 +253,29 @@ describe('Settlement printable page', () => {
       }
     ]
 
-    let monthsTooltip = '2024-10,2024-11'
+    // deleteInvoice({businessId, period: grossIncomes[0].period})
+
+    grossIncomes.forEach( g => {
+      createGrossIncome({
+        businessId,
+        grossIncome: g,
+        override: true,
+        deleteAfter: false
+      })
+    })
 
     createInvoice({
       businessId,
       grossIncomes,
       deleteAfter: true,
-
       override: true
+    })
+
+    grossIncomes.forEach((g) => {
+      deleteDeclaration({
+        businessId,
+        period: g.period
+      })
     })
   })
 })
